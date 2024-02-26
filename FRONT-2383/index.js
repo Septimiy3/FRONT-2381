5018c360e71e140f5a08f8f3b1ff75046ca24f30
Dmitry Kalabin <septimiy64@gmal.com>
2024-02-26 22:49:31 +0800

    fix delete

---
+++ FRONT-2383/index.js
@@ -0,0 +1,12 @@
+btnAddJs = document.querySelector('.addJsBtn')
+h2Js = document.querySelector('.h2Js')
+test = document.querySelector('.test-btn')
+
+
+btnAddJs.addEventListener('click', () => {
+    alert('Данный скрипо подключён в index.js')
+})
+h2Js.addEventListener('click', () => {
+    h2Js.style.color = 'Orange'
+    h2Js.style.fontSize = '20px'
+})
