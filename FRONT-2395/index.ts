0ff9ef0dc0c5fd5ef132b35b400df3228c73e473
Dmitry Kalabin <septimiy64@gmal.com>
2024-02-26 13:36:25 +0800

    add task FRONT-2395

---
+++ FRONT-2395/index.ts
@@ -0,0 +1,27 @@
+const btn = document.querySelector('.main-btn');
+
+function logMessage(message: string): void {
+    console.log(message);
+}
+
+function greet(name: string, greeting: string = 'Привет'): string {
+    return `${greeting}, ${name}!`;
+}
+
+function createPerson(name: string, age: number): { name: string, age: number } {
+    return { name, age };
+}
+function divideNumbers(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+}
+
+btn.addEventListener('click',()=> {
+    console.log('тест')
+    console.log(greet('Petter'))
+    console.log(createPerson('Mark',12))
+    console.log(divideNumbers(1,2))
+    logMessage('Привет')
+})
