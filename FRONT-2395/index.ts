fdb7c0b3e2d57d13e06f60342af9731c81c44aaf
Dmitry Kalabin <septimiy64@gmal.com>
2024-02-26 20:43:12 +0800

    fix func

--- FRONT-2395/index.ts
+++ FRONT-2395/index.ts
@@ -11,8 +11,8 @@ function greet(name: string, greeting: string = 'Привет'): string {
 function createPerson(name: string, age: number): { name: string, age: number } {
     return { name, age };
 }
-function divideNumbers(a: number, b: number): number {
-    if (b === 0) {
+function divideNumbers(a: string | number, b: string | number)  {
+    if (typeof a === 'string' || typeof b === 'string') {
         throw new Error("Division by zero is not allowed.");
     }
     return a / b;
@@ -22,6 +22,6 @@ btn.addEventListener('click',()=> {
     console.log('тест')
     console.log(greet('Petter'))
     console.log(createPerson('Mark',12))
-    console.log(divideNumbers('sdsd','sddd'))
+    console.log(divideNumbers(4,1))
     logMessage('Привет')
 })
