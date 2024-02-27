def0e8ad90da705a99427e8a2427ebef9901fae3
Dmitry Kalabin <septimiy64@gmal.com>
2024-02-27 14:58:37 +0800

    fix func

--- FRONT-2395/index.ts
+++ FRONT-2395/index.ts
@@ -11,18 +11,17 @@ function greet(name: string, greeting: string = 'Привет'): string {
 function createPerson(name: string, age: number): { name: string, age: number } {
     return { name, age };
 }
-function divideNumbers(a: number, b: number)  {
+function divideNumbers(a: number, b: number):number | never   {
     if (typeof a !== 'number' || typeof b !== 'number' || isNaN(a) || isNaN(b) || b === 0) {
-        throw new Error("Неверные параметры или деление на ноль не допускается.");
+            throw new Error("Неверные параметры или деление на ноль не допускается.")
     }
     return a / b;
 }
-
 
 btn.addEventListener('click',()=> {
     console.log('тест')
     console.log(greet('Petter'))
     console.log(createPerson('Mark',12))
-    console.log(divideNumbers(4,1))
+    console.log(divideNumbers(true,1))
     logMessage('Привет')
 })
